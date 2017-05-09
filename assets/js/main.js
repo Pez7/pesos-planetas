8332725882972792e7060f7f7c76b393aab0f758
Maria Paz <maria.pazrojasc@gmail.com>
2017-05-09 00:15:12 -0300

    funciones que calculan el peso

---
+++ assets/js/main.js
@@ -0,0 +1,70 @@
+function pesoPluton(peso){
+	var peso = document.getElementById("peso").value
+	var gravedad = 0.40;
+	var calculo = peso * gravedad;
+	var nuevoPeso = calculo.toFixed(2);
+
+	return alert("Tu peso en Plutón es de: " + nuevoPeso + " kilos.");
+}
+
+function pesoNeptuno(peso){
+	var peso = document.getElementById("peso").value
+	var gravedad = 11;
+	var calculo = peso * gravedad;
+
+	return alert("Tu peso en Neptuno es de: " + calculo + " kilos.");
+}
+
+function pesoUrano(peso){
+	var peso = document.getElementById("peso").value
+	var gravedad = 7.77;
+	var calculo = peso * gravedad;
+	var nuevoPeso = calculo.toFixed(2);
+
+	return alert("Tu peso en Urano es de: " + nuevoPeso + " kilos.");
+}
+
+function pesoSaturno(peso){
+	var peso = document.getElementById("peso").value
+	var gravedad = 9.05;
+	var calculo = peso * gravedad;
+	var nuevoPeso = calculo.toFixed(2);
+
+	return alert("Tu peso en Saturno es de: " + nuevoPeso + " kilos.");
+}
+
+function pesoJupiter(peso){
+	var peso = document.getElementById("peso").value
+	var gravedad = 22.88;
+	var calculo = peso * gravedad;
+	var nuevoPeso = calculo.toFixed(2);
+
+	return alert("Tu peso en Júpiter es de: " + nuevoPeso + " kilos.");
+}
+
+function pesoMarte(peso){
+	var peso = document.getElementById("peso").value
+	var gravedad = 3.72;
+	var calculo = peso * gravedad;
+	var nuevoPeso = calculo.toFixed(2);
+
+	return alert("Tu peso en Marte es de: " + nuevoPeso + " kilos.");
+}
+
+function pesoVenus(peso){
+	var peso = document.getElementById("peso").value
+	var gravedad = 8.87;
+	var calculo = peso * gravedad;
+	var nuevoPeso = calculo.toFixed(2);
+
+	return alert("Tu peso en Venus es de: " + nuevoPeso + " kilos.");
+}
+
+function pesoMercurio(peso){
+	var peso = document.getElementById("peso").value
+	var gravedad = 2.78;
+	var calculo = peso * gravedad;
+	var nuevoPeso = calculo.toFixed(2);
+
+	return alert("Tu peso en Mercurio es de: " + nuevoPeso + " kilos.");
+}
